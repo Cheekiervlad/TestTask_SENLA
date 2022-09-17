5cf5944ab106ff53af53c8619f86a6f3726555ae
Nadia920 <kabaknada64@gmail.com>
2022-09-17 12:59:20 +0300

    Created controller for queries from priducts

--- main/java/com/example/demo/controller/MainController.java
+++ main/java/com/example/demo/controller/MainController.java
@@ -1,6 +1,26 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.serviceImpl.ProductsServiceImpl;
 
+@Controller
+@GetMapping("/")
 public class MainController {
+    ProductsServiceImpl  productsServiceImpl;
+    @GetMapping("createProduct")
+    void create(){
+        productsServiceImpl.create();
+    }
+    @GetMapping("editProduct")
+    void edit(){
+        productsServiceImpl.edit();
+    }
+    @GetMapping("editProduct")
+    void delete(){
+        productsServiceImpl.delete();
+    }
+    @GetMapping("allProducts")
+    void allProducts(){
+        productsServiceImpl.all();
+    }
 
 }
--- main/java/com/example/demo/service/serviceImpl/ProductsServiceImpl.java
+++ main/java/com/example/demo/service/serviceImpl/ProductsServiceImpl.java
@@ -1,4 +1,20 @@
 package com.example.demo.service.serviceImpl;
+
+import com.example.demo.repository.ProductsRepository;
+
 @Service
 public class ProductsServiceImpl {
+    ProductsRepository  productsRepository;
+    public void create(){
+        productsRepository.createProduct();
+    }
+    public void edit(){
+        productsRepository.editProduct();
+    }
+    public void delete(){
+        productsRepository.deleteProduct();
+    }
+    public void all(){
+        productsRepository.allProducts();
+    }
 }
